3c75e35658e71ffde6f89f52149c0b08e1a4f364
prettyleka <prettyleka@gmail.com>
2022-12-11 18:00:16 +0200

    changes

--- src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
+++ src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
@@ -38,7 +38,8 @@ public class BrowserDriverFactory {
 			// Make sure to upgrade chromedriver to work with your browser version: https://chromedriver.chromium.org/downloads
 			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			//System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-			driver.set(new ChromeDriverManager().create());
+			WebDriverManager.chromedriver().setup();
+			driver.set(new ChromeDriver());
 
 			break;
 
