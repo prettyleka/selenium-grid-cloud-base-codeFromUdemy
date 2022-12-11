44f1c41705c32522d449b769e0194dc6d25866d7
prettyleka <prettyleka@gmail.com>
2022-12-11 20:56:59 +0200

    changes

--- src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
+++ src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
@@ -11,7 +11,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserDriverFactory {
 
-	private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	WebDriver driver;
+
+	//private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 	private String browser;
 	private Logger log;
 
@@ -29,9 +31,7 @@ public class BrowserDriverFactory {
 			// Make sure to upgrade chromedriver to work with your browser version: https://chromedriver.chromium.org/downloads
 			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			//System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-
-			driver.set(WebDriverManager.chromedriver().create());
-
+			driver = WebDriverManager.chromedriver().create();
 			break;
 
 		case "firefox":
@@ -39,17 +39,18 @@ public class BrowserDriverFactory {
 			//System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
 			//System.setProperty(FirefoxDriver.Capability.MARIONETTE, "true");
 			//System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
-			driver.set(new FirefoxDriver());
+			driver = WebDriverManager.firefoxdriver().create();
+
 			break;
 
 		default:
 			/*log.debug("Do not know how to start: " + browser + ", starting chrome.");
 			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");*/
-			driver.set(new ChromeDriver());
+			driver = WebDriverManager.chromedriver().create();
 			break;
 		}
 		java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
-		return driver.get();
+		return driver;
 	}
 }
